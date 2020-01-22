b417faf2ea9582aba718bc0ef49e9c78881c648e
EismanW <pl.amosso@gmail.com>
2020-01-22 15:06:34 +0000

    Added method for getting the list of whiskies based on year

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,31 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
 
+	@Autowired
+	private WhiskyRepository whiskyRepository;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void getWhiskyByYear() {
+		List<Whisky> whiskies = whiskyRepository.findAllByYear(1991);
+		assertEquals("The Rosebank 12 - Flora and Fona", whiskies.get(0).getName());
+	}
 }
