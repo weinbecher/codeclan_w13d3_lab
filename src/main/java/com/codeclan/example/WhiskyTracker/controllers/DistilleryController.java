3783987f7015fb5d4977b0527d87951a8118a8d8
EismanW <pl.amosso@gmail.com>
2020-01-22 15:21:05 +0000

    Added rest method to get distilleries by region

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,11 +1,24 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/distilleries")
 public class DistilleryController {
 
+    @Autowired
+    private DistilleryRepository distilleryRepository;
 
+    @GetMapping(value = "/region/{region}")
+    public List<Distillery> findDistilleriesByRegion(@PathVariable String region) {
+        return distilleryRepository.findAllByRegion(region);
+    }
 }
