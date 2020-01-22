98a53c9c25cf81c4a81d89f8b3d35a3ab4a08102
EismanW <pl.amosso@gmail.com>
2020-01-22 15:12:09 +0000

    Added whisky controller for getting whiskies by year and added json ignore for distillery whiskies

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,24 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/whiskies")
 public class WhiskyController {
 
+    @Autowired
+    private WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/year/{year}")
+    public List<Whisky> findWhiskysByYear(@PathVariable int year) {
+        return whiskyRepository.findAllByYear(year);
+    }
 }
